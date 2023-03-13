d6904b02b6e859d6170327ae8ac5d493335f276a
Hussam Al Basha <hoa12@mail.aub.edu>
2023-03-13 21:26:44 +0200

    created ProfileNotFoundException

---
+++ Exceptions/ProfileNotFound.cs
@@ -0,0 +1,14 @@
+﻿namespace CarWebsiteBackend.Exceptions
+{
+    public class ProfileNotFound: Exception
+    {
+        //Overriding the Message property
+        public override string Message
+        {
+            get
+            {
+                return "ProfileNotFound";
+            }
+        }
+    }
+}
