3d11449a163db76731dc4b7b616ed12a1e7f715d
Chris_amV <csa23@mail.aub.edu>
2023-03-16 00:00:02 +0200

    modified:   Controllers/AccountController.cs 	modified:   Interfaces/IAccountInterface.cs 	modified:   Storage/InMemoryStore.cs

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -28,4 +28,35 @@ public class AccountController : ControllerBase
             return BadRequest("Email already taken. Sign up with another email, or sign in.");
         }
     }
+
+    [HttpGet("get/{address}")]
+        public async Task<ActionResult<Account>> GetAccounts(string address)
+        {
+            Email email = new(address);
+            var account = await accountInterface.GetAccount(email);
+            if (account == null)
+            {
+                return NotFound();
+            }
+                return account;
+        }
+
+    [HttpDelete("delete/{address}")]
+        public async Task<IActionResult> DeleteAccountsItem(string address)
+        {
+            Email email = new(address);
+            var account = await accountInterface.GetAccount(email);
+            if (email == null)
+            {
+                return NotFound();
+            }
+            try{
+                await accountInterface.DeleteAccount(email);
+                return NoContent();
+            }
+            catch{
+                return BadRequest("Email doesn't exist.");
+            }
+
+        }
 }
--- Storage/InMemoryStore.cs
+++ Storage/InMemoryStore.cs
@@ -1,5 +1,6 @@
 ﻿using CarWebsiteBackend.Interfaces;
 using CarWebsiteBackend.DTOs;
+using Microsoft.AspNetCore.Mvc;
 namespace CarWebsiteBackend.Storage;
 
 public class InMemoryStore : IAccountInterface
@@ -15,5 +16,21 @@ public class InMemoryStore : IAccountInterface
         }
 
         throw new ArgumentException("Email already taken. Sign up with another email, or sign in.");
+    }
+    public Task <Account?> GetAccount(Email email){
+        if (!accountDict.ContainsKey(email))
+        {
+            throw new ArgumentException("This email doesn't exist!");
+        }
+        return Task.FromResult<Account?>(accountDict[email]);
+    }
+
+    public Task DeleteAccount(Email email){
+        if (!accountDict.ContainsKey(email))
+        {
+            throw new ArgumentException("This email doesn't exist!");
+        }
+        accountDict.Remove(email);
+        return Task.CompletedTask;
     }
 }
