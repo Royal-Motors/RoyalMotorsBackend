ac97941a0ad3cd6fc4329dccb8193c8c51dc81c3
Hassan Baydoun <hhb16@mail.aub.edu>
2023-03-17 23:06:58 +0200

    Hassan Baydoun: Database Done, everything working :) ~H

--- Program.cs
+++ Program.cs
@@ -1,16 +1,20 @@
 using CarWebsiteBackend.Controllers;
+using CarWebsiteBackend.Data;
 using CarWebsiteBackend.Interfaces;
 using CarWebsiteBackend.Storage;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection")));
+builder.Services.AddScoped<IAccountInterface, AccountStorage>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddSingleton<IAccountInterface, InMemoryStore>();
+//builder.Services.AddSingleton<IAccountInterface, InMemoryStore>();
 
 
 var app = builder.Build();
---
+++ Storage/AccountStorage.cs
@@ -0,0 +1,71 @@
+﻿using CarWebsiteBackend.Data;
+using CarWebsiteBackend.DTOs;
+using CarWebsiteBackend.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using CarWebsiteBackend.Exceptions;
+using Microsoft.Data.SqlClient;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarWebsiteBackend.Storage
+{
+    public class AccountStorage : IAccountInterface
+    {
+
+        private readonly DataContext _context;
+        public AccountStorage(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAccount(Account account)
+        {
+            try
+            {
+                _context.Add(account);
+                var saved = await _context.SaveChangesAsync();
+            }
+            catch
+            {
+                throw new ProfileAlreadyExistsException();
+            }
+        }
+
+        public async Task DeleteAccount(string email)
+        {   
+            var sql = "DELETE FROM Accounts WHERE email = @Email";
+            var parameters = new[] { new SqlParameter("@Email", email) };
+            var rowsAffected = await _context.Database.ExecuteSqlRawAsync(sql, parameters);
+            if (rowsAffected <= 0) 
+            {
+                throw new ProfileNotFoundException();
+            }
+        }
+
+        public async Task<Account> GetAccount(string email)
+        {
+            var Acc = await _context.Accounts.Where(p => p.email == email).FirstOrDefaultAsync();
+            if (Acc == null)
+            {
+                throw new ProfileNotFoundException();
+            }
+            return Acc;
+        }
+
+        public async Task ReplaceAccount(Account account)
+        {
+            var sql = "UPDATE Accounts SET firstname = @NewFirstname, lastname = @NewLastname, password = @NewPassword WHERE email = @Email";
+            var parameters = new[]
+            {
+            new SqlParameter("@NewFirstname", account.firstname),
+            new SqlParameter("@NewLastname", account.lastname),
+            new SqlParameter("@NewPassword", account.password),
+            new SqlParameter("@Email", account.email)
+            };
+            var rowsAffected = await _context.Database.ExecuteSqlRawAsync(sql, parameters);
+            if(rowsAffected <= 0)
+            {
+                throw new ProfileNotFoundException();
+            }
+        }
+    }
+}
