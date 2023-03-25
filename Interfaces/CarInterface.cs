260cd875336ee89f5f28cbc819e2c293560b5f04
Hussam Al Basha <hoa12@mail.aub.edu>
2023-03-25 21:57:34 +0200

    Implement Car Controller (Add/Get/Edit/Delete) & created CarInterface

---
+++ Interfaces/CarInterface.cs
@@ -0,0 +1,11 @@
+﻿using CarWebsiteBackend.DTOs;
+using Microsoft.AspNetCore.Mvc;
+namespace CarWebsiteBackend.Interfaces;
+
+public interface CarInterface
+{
+    Task AddCar(Car car);            // Used for adding a car, returns error if car already exists
+    Task DeleteCar(string name);
+    Task EditCar(Car car);           // Forces upsert, used for Edit car
+    Task<Car?> GetCar(string name);
+}
