a1c16c4ac8b5fc91d849a9962cdacab9fcf84ef0
Hussam Al Basha <hoa12@mail.aub.edu>
2023-03-25 22:06:46 +0200

    Created CarStorage.cs

---
+++ Storage/CarStorage.cs
@@ -0,0 +1,44 @@
+﻿using CarWebsiteBackend.Data;
+using CarWebsiteBackend.DTOs;
+using CarWebsiteBackend.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using CarWebsiteBackend.Exceptions.ProfileExceptions;
+using CarWebsiteBackend.Exceptions.CarExceptions;
+using Microsoft.Data.SqlClient;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarWebsiteBackend.Storage
+{
+    /*
+    public class CarStorage : CarInterface
+    {
+        
+        private readonly DataContext _context;
+        public CarStorage(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddCar(Car car)
+        {
+            
+        }
+
+        public async Task DeleteCar(string name)
+        {
+            
+        }
+
+        public async Task<Car> GetCar(string name)
+        {
+            
+        }
+
+        public async Task EditCar(Car car)
+        {
+           
+        }
+        
+    }
+    */
+}
