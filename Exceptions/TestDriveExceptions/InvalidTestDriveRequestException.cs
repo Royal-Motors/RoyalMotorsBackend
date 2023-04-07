03e104e04e9680fce9dc1e8fe36eca5599d976c6
Hussam Al Basha <hoa12@mail.aub.edu>
2023-04-07 17:07:46 +0300

    Implement TestDriveExceptions

---
+++ Exceptions/TestDriveExceptions/InvalidTestDriveRequestException.cs
@@ -0,0 +1,16 @@
+﻿namespace CarWebsiteBackend.Exceptions.TestDriveExceptions
+{
+    //This exception can be thrown if the user submits an invalid or incomplete test drive request
+    public class InvalidTestDriveRequestException : Exception
+    {
+        //Overriding the Message property
+        public override string Message
+        {
+            get
+            {
+                return "Invalid Test Drive Request";
+            }
+        }
+    }
+}
+
