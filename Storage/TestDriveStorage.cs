c37497347380b9c63f74b17e9939f35265bb8993
Hussam Al Basha <hoa12@mail.aub.edu>
2023-04-09 20:48:40 +0300

    Edited Itestdrive interface and created testdrive storage

---
+++ Storage/TestDriveStorage.cs
@@ -0,0 +1,52 @@
+﻿using CarWebsiteBackend.Data;
+using CarWebsiteBackend.DTOs;
+using CarWebsiteBackend.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using CarWebsiteBackend.Exceptions.CarExceptions;
+using Microsoft.Data.SqlClient;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarWebsiteBackend.Storage
+{
+    //public class TestDriveStorage : ITestDriveInterface
+    //{
+    //    private readonly DataContext _context;
+    //    public TestDriveStorage(DataContext context)
+    //    {
+    //        _context = context;
+    //    }
+
+    //    public async Task AddTestDrive(TestDrive test_drive)
+    //    {
+    //        try
+    //        {
+    //            _context.Add(test_drive);
+    //            var saved = await _context.SaveChangesAsync();
+    //        }
+    //        catch
+    //        {
+    //            throw new DuplicateCarException();
+    //        }
+    //    }
+
+    //    public async Task DeleteTestDrive(int Id)
+    //    {
+            
+    //    }
+
+    //    public async Task<TestDrive> GetTestDriveByTestDriveId(int Id)
+    //    {
+
+    //    }
+
+    //    public async Task<List<TestDrive>> GetTestDriveByCarId(int Car_Id)
+    //    {
+
+    //    }
+    //    public async Task<List<TestDrive>> GetTestDriveByAccount(int AccountId)
+    //    {
+
+    //    }
+
+    //}
+}
