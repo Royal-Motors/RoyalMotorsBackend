4e9d2637f5fb8736cc5008c913658358f6e3ad45
Hussam Al Basha <hoa12@mail.aub.edu>
2023-04-14 15:42:03 +0300

    Created Startup.cs and edited CarController.cs to [EnableCors("AllowAnyOrigin")]

---
+++ Startup.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CarWebsiteBackend
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // add CORS policy to allow requests from any origin
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAnyOrigin",
+                    builder => builder.AllowAnyOrigin()
+                                      .AllowAnyHeader()
+                                      .AllowAnyMethod());
+            });
+
+            // configure your other services here
+            // ...
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseCors("AllowAnyOrigin");
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
