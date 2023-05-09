17d2adc753854a233d8409f7e7604536dbf36b62
HussameddineAlAttar <hya15@mail.aub.edu>
2023-05-10 02:09:36 +0300

    Fix "Add sale" functionality

--- Storage/CarStorage.cs
+++ Storage/CarStorage.cs
@@ -4,98 +4,96 @@ using CarWebsiteBackend.Interfaces;
 using Microsoft.EntityFrameworkCore;
 using CarWebsiteBackend.Exceptions.CarExceptions;
 using Microsoft.Data.SqlClient;
-using Microsoft.AspNetCore.Mvc;
 
-namespace CarWebsiteBackend.Storage
+namespace CarWebsiteBackend.Storage;
+
+public class CarStorage : CarInterface
 {
-    public class CarStorage : CarInterface
+    private readonly DataContext _context;
+    public CarStorage(DataContext context)
     {
-        private readonly DataContext _context;
-        public CarStorage(DataContext context)
-        {
-            _context = context;
-        }
+        _context = context;
+    }
 
-        public async Task AddCar(Car car)
+    public async Task AddCar(Car car)
+    {
+        try
         {
-               try
-            {
-                _context.Add(car);
-                var saved = await _context.SaveChangesAsync();
-            }
-            catch
-            {
-                throw new DuplicateCarException();
-            } 
+            _context.Add(car);
+            await _context.SaveChangesAsync();
         }
-
-        public async Task DeleteCar(string name)
+        catch
         {
-            var sql = "DELETE FROM Cars WHERE name = @Name";
-            var parameters = new[] { new SqlParameter("@Name", name) };
-            var rowsAffected = await _context.Database.ExecuteSqlRawAsync(sql, parameters);
-            if (rowsAffected <= 0)
-            {
-                throw new CarNotFoundException();
-            }
+            throw new DuplicateCarException();
+        } 
+    }
+
+    public async Task DeleteCar(string name)
+    {
+        var sql = "DELETE FROM Cars WHERE name = @Name";
+        var parameters = new[] { new SqlParameter("@Name", name) };
+        var rowsAffected = await _context.Database.ExecuteSqlRawAsync(sql, parameters);
+        if (rowsAffected <= 0)
+        {
+            throw new CarNotFoundException();
         }
+    }
 
-        public async Task SellCar(string name)
+    public async Task SellCar(string name)
+    {
+        var sql = "DELETE FROM Cars WHERE name = @Name";
+        var parameters = new[] { new SqlParameter("@Name", name) };
+        var rowsAffected = await _context.Database.ExecuteSqlRawAsync(sql, parameters);
+        if (rowsAffected <= 0)
         {
-            var sql = "DELETE FROM Cars WHERE name = @Name";
-            var parameters = new[] { new SqlParameter("@Name", name) };
-            var rowsAffected = await _context.Database.ExecuteSqlRawAsync(sql, parameters);
-            if (rowsAffected <= 0)
-            {
-                throw new CarNotFoundException();
-            }
+            throw new CarNotFoundException();
         }
+    }
 
-        public async Task<Car> GetCar(string name)
+    public async Task<Car> GetCar(string name)
+    {
+        var Car = await _context.Cars.Where(p => p.name == name).FirstOrDefaultAsync();
+        if (Car == null)
         {
-            var Car = await _context.Cars.Where(p => p.name == name).FirstOrDefaultAsync();
-            if (Car == null)
-            {
-                throw new CarNotFoundException();
-            }
-            return Car;
+            throw new CarNotFoundException();
         }
+        return Car;
+    }
 
-        public async Task<List<Car>> GetAllCars()
+    public async Task<List<Car>> GetAllCars()
+    {
+        var cars = await _context.Cars.ToListAsync();
+        if (cars == null || cars.Count == 0)
         {
-            var cars = await _context.Cars.ToListAsync();
-            if (cars == null || cars.Count == 0)
-            {
-                throw new CarNotFoundException();
-            }
-            return cars;
+            throw new CarNotFoundException();
         }
+        return cars;
+    }
 
-        public async Task EditCar(Car car)
+    public async Task EditCar(Car car)
+    {
+        var sql = "UPDATE Cars SET make = @NewMake, model = @NewModel, year = @NewYear, color = @NewColor, price = @NewPrice, description = @NewDescription, mileage = @NewMileage, image_id_list = @NewImage, video_id = @NewVideo, fueltankcapacity = @Newfueltankcapacity, transmissiontype = @Newtransmissiontype, horsepower = @Newhorsepower , fuelconsumption = @Newfuelconsumption  WHERE name = @Name";
+        var parameters = new[]
         {
-            var sql = "UPDATE Cars SET make = @NewMake, model = @NewModel, year = @NewYear, color = @NewColor, price = @NewPrice, description = @NewDescription, mileage = @NewMileage, image_id_list = @NewImage, video_id = @NewVideo, fueltankcapacity = @Newfueltankcapacity, transmissiontype = @Newtransmissiontype, horsepower = @Newhorsepower , fuelconsumption = @Newfuelconsumption  WHERE name = @Name";
-            var parameters = new[]
-            {
-            new SqlParameter("@Name", car.name),
-            new SqlParameter("@NewMake", car.make),
-            new SqlParameter("@NewModel", car.model),
-            new SqlParameter("@NewYear", car.year),
-            new SqlParameter("@NewColor", car.color),
-            new SqlParameter("@NewPrice", car.price),
-            new SqlParameter("@NewDescription", car.description),
-            new SqlParameter("@NewMileage", car.mileage),
-            new SqlParameter("@NewImage", car.image_id_list),
-            new SqlParameter("@NewVideo", car.video_id),
-            new SqlParameter("@Newfueltankcapacity", car.fueltankcapacity),
-            new SqlParameter("@Newtransmissiontype", car.transmissiontype),
-            new SqlParameter("@Newhorsepower", car.horsepower),
-            new SqlParameter("@Newfuelconsumption", car.fuelconsumption)
-            };
-            var rowsAffected = await _context.Database.ExecuteSqlRawAsync(sql, parameters);
-            if(rowsAffected <= 0)
-            {
-                throw new CarNotFoundException();
-            }
+        new SqlParameter("@Name", car.name),
+        new SqlParameter("@NewMake", car.make),
+        new SqlParameter("@NewModel", car.model),
+        new SqlParameter("@NewYear", car.year),
+        new SqlParameter("@NewColor", car.color),
+        new SqlParameter("@NewPrice", car.price),
+        new SqlParameter("@NewDescription", car.description),
+        new SqlParameter("@NewMileage", car.mileage),
+        new SqlParameter("@NewImage", car.image_id_list),
+        new SqlParameter("@NewVideo", car.video_id),
+        new SqlParameter("@Newfueltankcapacity", car.fueltankcapacity),
+        new SqlParameter("@Newtransmissiontype", car.transmissiontype),
+        new SqlParameter("@Newhorsepower", car.horsepower),
+        new SqlParameter("@Newfuelconsumption", car.fuelconsumption)
+        };
+        var rowsAffected = await _context.Database.ExecuteSqlRawAsync(sql, parameters);
+        if(rowsAffected <= 0)
+        {
+            throw new CarNotFoundException();
         }
     }
 }
---
+++ Storage/SaleStorage.cs
@@ -0,0 +1,126 @@
+﻿using CarWebsiteBackend.Data;
+using CarWebsiteBackend.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using CarWebsiteBackend.Extensions;
+using CarWebsiteBackend.DTOs;
+
+namespace CarWebsiteBackend.Storage;
+
+public class SaleStorage : ISaleStore
+{
+    private readonly DataContext _context;
+    public SaleStorage(DataContext context)
+    {
+        _context = context;
+    }
+
+    public async Task AddSale(Sale sale)
+    {
+        _context.Add(sale);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<int> GetTotalCarsSold()
+    {
+        return await _context.Sales.CountAsync();
+    }
+
+    public async Task<int> GetTotalCarsSoldByDay(int unix_time)
+    {
+        var startOfDay = DateTimeOffset.FromUnixTimeSeconds(unix_time).Date;
+        var endOfDay = startOfDay.AddDays(1);
+        return await _context.Sales
+            .Where(s => s.Time >= startOfDay.ToUnixTimeSeconds() && s.Time < endOfDay.ToUnixTimeSeconds())
+            .CountAsync();
+    }
+
+    public async Task<int> GetTotalCarsSoldByMonth(int unix_time)
+    {
+        var startOfMonth = DateTimeOffset.FromUnixTimeSeconds(unix_time).Date;
+        var endOfMonth = startOfMonth.AddMonths(1);
+        return await _context.Sales
+            .Where(s => s.Time >= startOfMonth.ToUnixTimeSeconds() && s.Time < endOfMonth.ToUnixTimeSeconds())
+            .CountAsync();
+    }
+
+    public async Task<int> GetTotalCarsSoldByYear(int unix_time)
+    {
+        var startOfYear = DateTimeOffset.FromUnixTimeSeconds(unix_time).Date;
+        var endOfYear = startOfYear.AddYears(1);
+        return await _context.Sales
+            .Where(s => s.Time >= startOfYear.ToUnixTimeSeconds() && s.Time < endOfYear.ToUnixTimeSeconds())
+            .CountAsync();
+    }
+
+    public async Task<int> GetTotalCustomers()
+    {
+        return await _context.Sales.Select(s => s.Email).Distinct().CountAsync();
+    }
+
+    public async Task<int> GetTotalSales()
+    {
+        return await _context.Sales.SumAsync(s => s.Price);
+    }
+
+    public async Task<int> GetTotalSalesByDay(int unix_time)
+    {
+        var startOfDay = DateTimeOffset.FromUnixTimeSeconds(unix_time).Date;
+        var endOfDay = startOfDay.AddDays(1);
+        var time1 = startOfDay.ToUnixTimeSeconds();
+        var time2 = endOfDay.ToUnixTimeSeconds();
+
+        return await _context.Sales
+            .Where(s => s.Time >= time1 && s.Time < time2)
+            .SumAsync(s => s.Price);
+    }
+
+    public async Task<int> GetTotalSalesByMonth(int unix_time)
+    {
+        var startOfMonth = DateTimeOffset.FromUnixTimeSeconds(unix_time).Date;
+        var endOfMonth = startOfMonth.AddMonths(1);
+        return await _context.Sales
+            .Where(s => s.Time >= startOfMonth.ToUnixTimeSeconds() && s.Time < endOfMonth.ToUnixTimeSeconds())
+            .SumAsync(s => s.Price);
+    }
+
+    public async Task<int> GetTotalSalesByYear(int unix_time)
+    {
+        var startOfYear = DateTimeOffset.FromUnixTimeSeconds(unix_time).Date;
+        var endOfYear = startOfYear.AddYears(1);
+        return await _context.Sales
+            .Where(s => s.Time >= startOfYear.ToUnixTimeSeconds() && s.Time < endOfYear.ToUnixTimeSeconds())
+            .SumAsync(s => s.Price);
+    }
+
+    public async Task<int> GetTotalTestDriveByDay(int unix_time)
+    {
+        var startOfDay = DateTimeOffset.FromUnixTimeSeconds(unix_time).Date;
+        var endOfDay = startOfDay.AddDays(1);
+        return await _context.TestDrives
+            .Where(s => s.Time >= startOfDay.ToUnixTimeSeconds() && s.Time < endOfDay.ToUnixTimeSeconds())
+            .CountAsync();
+    }
+
+    public async Task<int> GetTotalTestDriveByMonth(int unix_time)
+    {
+        var startOfMonth = new DateTime(unix_time, 1, 1);
+        var endOfMonth = startOfMonth.AddMonths(1);
+        return await _context.TestDrives
+            .Where(s => s.Time >= startOfMonth.ToUnixTimeSeconds() && s.Time < endOfMonth.ToUnixTimeSeconds())
+            .CountAsync();
+    }
+
+    public async Task<int> GetTotalTestDriveByYear(int unix_time)
+    {
+        var startOfYear = new DateTime(unix_time, 1, 1);
+        var endOfYear = startOfYear.AddYears(1);
+        return await _context.TestDrives
+            .Where(s => s.Time >= startOfYear.ToUnixTimeSeconds() && s.Time < endOfYear.ToUnixTimeSeconds())
+            .CountAsync();
+    }
+
+    public async Task<int> GetTotalTestDriveRequsted()
+    {
+        return await _context.TestDrives.CountAsync();
+    }
+}
