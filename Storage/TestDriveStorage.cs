27948e120894dcb9ff127e335b09e9ab17cdd0d8
Hassan Baydoun <hhb16@mail.aub.edu>
2023-05-15 17:19:09 +0300

    timeslotGenerator

--- Storage/TestDriveStorage.cs
+++ Storage/TestDriveStorage.cs
@@ -128,5 +128,73 @@ namespace CarWebsiteBackend.Storage
             }
             return testDrives;
         }
+
+        public async Task<List<long>> GetAllAvailableSlots(string CarName)
+        {
+            var testDrives = await _context.TestDrives
+                .Include(td => td.Car)
+                .Include(td => td.Account)
+                .ToListAsync();
+            var testDrivesCar = await _context.TestDrives
+                .Include(td => td.Account)
+                .Where(td => td.Car.name == CarName)
+                .ToListAsync();
+            var slots = timeSlotGenerator();
+            foreach (var test_drive in testDrives)
+            {
+                if (slots.Contains(test_drive.Time))
+                {
+                    slots.Remove(test_drive.Time);
+                }
+            }
+            foreach (var test_drive_car in testDrivesCar)
+            {
+                if (slots.Contains(test_drive_car.Time))
+                {
+                    slots.Remove(test_drive_car.Time);
+                }
+            }
+            List<long> uniqueSlots = slots.GroupBy(x => x).Select(x => x.Key).ToList();
+            return uniqueSlots;
+        }
+
+        public static List<long> timeSlotGenerator()
+        {
+            TimeZoneInfo timeZone = TimeZoneInfo.FindSystemTimeZoneById("GMT Standard Time");
+
+            // Create an empty list to hold the time slots
+            List<long> timeSlots = new List<long>();
+
+            // Loop through the upcoming week
+            for (int i = 0; i < 7; i++)
+            {
+                // Get the current date in the local time zone
+                DateTime currentDate = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow.AddDays(i + 1).Date, timeZone);
+                // Set the start time to the beginning of next day at 10am in the local time zone
+                DateTime startTime = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow.AddDays(i + 1).Date.AddHours(6), timeZone);
+                // Set the end time to 5pm in the local time zone
+                DateTime endTime = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow.AddDays(i + 1).Date.AddHours(13), timeZone);
+
+                // Check if the current day is not a weekend day (Saturday or Sunday)
+                if (currentDate.DayOfWeek != DayOfWeek.Saturday && currentDate.DayOfWeek != DayOfWeek.Sunday)
+                {
+
+                    // Loop through the time slots from the start time to the end time with a 1-hour step
+                    for (DateTime currentSlot = startTime; currentSlot < endTime; currentSlot = currentSlot.AddHours(1))
+                    {
+                        // Convert the current time slot to Unix time
+                        long unixTime = (long)(currentSlot.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+
+                        // Add the Unix time to the list of time slots
+                        timeSlots.Add(unixTime);
+                        timeSlots.Add(unixTime);
+                    }
+                }
+            }
+            // Convert the list of time slots to an array
+            long[] timeSlotsArray = timeSlots.ToArray();
+
+            return timeSlots;
+        }
     }
 }
